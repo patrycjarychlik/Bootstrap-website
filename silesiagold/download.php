c269f12ec23dcda872821f0e272a61ccaa510b49
Patrycja Rychlik <rychlikpatrycja@gmail.com>
2015-08-28 23:11:36 +0200

    new version

---
+++ silesiagold/download.php
@@ -0,0 +1,57 @@
+<?php
+$the_folder = 'galeria/dokumenty';
+$zip_file_name = 'dokumentySilegold.zip';
+
+
+$download_file= true;
+//$delete_file_after_download= true; doesnt work!!
+
+
+class FlxZipArchive extends ZipArchive {
+
+    public function addDir($location, $name) {
+        $this->addEmptyDir($name);
+
+        $this->addDirDo($location, $name);
+     } // EO addDir;
+
+    private function addDirDo($location, $name) {
+        $name .= '/';
+        $location .= '/';
+
+        // Read all Files in Dir
+        $dir = opendir ($location);
+        while ($file = readdir($dir))
+        {
+            if ($file == '.' || $file == '..') continue;
+            // Rekursiv, If dir: FlxZipArchive::addDir(), else ::File();
+            $do = (filetype( $location . $file) == 'dir') ? 'addDir' : 'addFile';
+            $this->$do($location . $file, $name . $file);
+        }
+    } // EO addDirDo();
+}
+
+$za = new FlxZipArchive;
+$res = $za->open($zip_file_name, ZipArchive::CREATE);
+if($res === TRUE) 
+{
+    $za->addDir($the_folder, basename($the_folder));
+    $za->close();
+}
+else  { echo 'Nie można';}
+
+if ($download_file)
+{
+    ob_get_clean();
+    header("Pragma: public");
+    header("Expires: 0");
+    header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+    header("Cache-Control: private", false);
+    header("Content-Type: application/zip");
+    header("Content-Disposition: attachment; filename=" . basename($zip_file_name) . ";" );
+    header("Content-Transfer-Encoding: binary");
+    header("Content-Length: " . filesize($zip_file_name));
+    readfile($zip_file_name);
+
+}
+?>
--- silesiagold/mail.php
+++ silesiagold/mail.php
@@ -1,17 +1,33 @@
-<?
- $temat=$_POST['temat'];
- $tresc=$_POST['tresc'];
- $email=$_POST['email'];
- if (!strstr($email, "@"))$email=$email."@to.jest.nr.telefonu";
-    $naglowki  = "MIME-Version: 1.0\r\n";
-    $naglowki .= "Content-type: text/html; charset=utf-8\r\n";
-    $naglowki .= "Content-Transfer-Encoding: 8bit\r\n";
-    $naglowki .= "From: $email";
- 
- mail("zdzichs@caprisoft.com.pl","WWW: ".$temat,$tresc,$naglowki);
-
- header("Location: http://www.silegold.pl/?tytul=pomail");
- die();
+<?php
+$do = $_POST ['dropdown'];
+
+$temat = $_POST ['temat'];
+
+$tresc = $_POST ['tresc'];
+
+$email = $_POST ['email'];
+
+$do = str_replace ( 'AT', '@', $do );
+
+if (! strstr ( $email, "@" ))
+	$email = $email . "@to.jest.nr.telefonu";
+
+$naglowki = "MIME-Version: 1.0\r\n";
+
+$naglowki .= "Content-type: text/html; charset=utf-8\r\n";
+
+$naglowki .= "Content-Transfer-Encoding: 8bit\r\n";
+
+$naglowki .= "From: $email";
+
+// ZMIANA E-MAIL - należy przetestować
+mail($do ,"WWW: ".$temat,$tresc,$naglowki);
+// mail ( "zdzichs@caprisoft.com.pl", "WWW: " . $temat, $tresc, $naglowki );
+
+header ( "Location: http://www.silegold.pl/?tytul=pomail" );
+
+die ();
+
 ?>
 
 
